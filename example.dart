b83824a02c08609de90c235fd15b6d54ccb01cce
Kausar Moshood <kausarmoshood@gmail.com>
2023-02-27 18:14:44 +0100

    first commit

---
+++ example.dart
@@ -0,0 +1,62 @@
+import 'dart:io';
+
+void main() {
+  // print('Enter your name');
+  // String? name = stdin.readLineSync();
+  // print('Welcome to this session $name');
+  print('Enter a number');
+  String? num1 = stdin.readLineSync();
+  stdout.write('Enter the second number');
+  String? num2 = stdin.readLineSync();
+
+ print('The two numbers inputed  are $num1 and $num2');
+
+
+//   print('Welcome to today Bootcamp');
+//   /*This is a single line comment
+//   This is a single line comment*/
+
+//   ///This is a single line comment This is a single line comment///
+//   ///
+//   ///
+//   int number = 4;
+//   double number2 = 5.0;
+//   num num3 = 7;
+
+//   var addition = number + num3;
+//   print('$addition');
+
+//   String name = 'Kausar'; // Single quote
+//   String fname = "Moshood"; //Double quote
+
+//   String str = 'Fun';
+//   String str1 = 'Fun';
+
+//   bool val = (str == str1);
+//   print(val);
+
+//   List item = ['Kausar', 19, 'Lagos'];
+//   print(item[2]);
+//   Map<dynamic, String> items = {'name': 'Kausar', 19: 'one'};
+//   print(items[19]);
+
+//   int num1 = 22;
+//   int num2 = 22;
+//   //Arithmetic Operators
+//   print(num1 + num2); //addition
+//   print(num1 - num2); //subtraction
+//   print(num1 * num2); //multiplication
+//   print(num1 ~/ num2); //output should be int
+//   print(num1 / num2); //division
+//   print(num1 % num2); //modulus
+
+//  //Relational Operators
+//   print(num1 > num2); // greater than
+//   print(num1 < num2); // less than
+//   print(num1 >= num2); // greater than or equal to
+//   print(num1 <= num2); // less than or equal to
+//   print(num1 == num2); // equal to
+//   print(num1 != num2); // not equal to
+}
+
+// int int = 7;
